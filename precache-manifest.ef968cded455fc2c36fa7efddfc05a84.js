60f7b40c760839443899fe1f89d9f98616802fc4
Damon Anthony <damonanthony125@gmail.com>
2020-07-02 11:11:03 +0200

    Updates

---
+++ precache-manifest.ef968cded455fc2c36fa7efddfc05a84.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c29d2830d5bc67b7859940b52b291b2",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "9a3e6f7c4bb1a89a9189",
+    "url": "/Portfolio/static/css/2.9dff4bae.chunk.css"
+  },
+  {
+    "revision": "838bae50c38f58144b3e",
+    "url": "/Portfolio/static/css/main.5459ccbb.chunk.css"
+  },
+  {
+    "revision": "9a3e6f7c4bb1a89a9189",
+    "url": "/Portfolio/static/js/2.db7d1b91.chunk.js"
+  },
+  {
+    "revision": "69c93b8d82e1fb02f01877bde6f39bab",
+    "url": "/Portfolio/static/js/2.db7d1b91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "838bae50c38f58144b3e",
+    "url": "/Portfolio/static/js/main.c6b6440f.chunk.js"
+  },
+  {
+    "revision": "3bd9cfc7f0b9814341f4",
+    "url": "/Portfolio/static/js/runtime-main.f557120a.js"
+  },
+  {
+    "revision": "1d8bf7399ae0fdb549cdf7840b1766fd",
+    "url": "/Portfolio/static/media/background.1d8bf739.jpg"
+  }
+]);
